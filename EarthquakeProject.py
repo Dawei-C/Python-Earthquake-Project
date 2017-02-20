98d172e4e02035357fc01546f975170d75d9ddf6
Dawei-C <dawei.chen@baruchmail.cuny.edu>
2017-02-20 01:00:23 -0500

    Upload updated project.

--- EarthquakeProject.py
+++ EarthquakeProject.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+from operator import itemgetter
 
 fp = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/"
 fn = "all_week.csv"
@@ -11,13 +12,32 @@ f.close()
 with open(fn) as csvfile:
     reader = csv.DictReader(csvfile)
     magnitudeList = []
+    placeList = []
+    noInformation = []
+
     for row in reader:
+        # skip rows that have no data on magnitude
+        if row['mag'] == "":
+            noInformation.append(row['place'])
+            continue
+
+        placeList.append(row['place'])
         magnitudeList.append(float(row['mag']))
         print("Location: " + row['place'] + " | Magnitude: " + row['mag'] + " | Time: " + row['time'])
 
+    print()
+    for i in noInformation:
+        print(i + "'s magnitude has not been disclosed yet.")
+
+    placeMag = list(zip(placeList, magnitudeList))
+    placeMag = sorted(placeMag, key=itemgetter(1))
+    topMag = placeMag[-1]
     averageMag = sum(magnitudeList)/len(magnitudeList)
+    totalEQ = len(magnitudeList)
+    documentedEQ = totalEQ - len(noInformation)
 
 print()
-print("There were " + str(len(magnitudeList)) + " earthquakes this week.")
-print("The average magnitude is " + '{0:.2f}'.format(averageMag) + ".")
-print("The highest magnitude was " + str(max(magnitudeList)) + ".")
+print("There were " + str(totalEQ) + " earthquakes within the past 7 days.")
+print(str(documentedEQ) + " had magnitude values that have been documented.")
+print("The average magnitude was " + '{0:.2f}'.format(averageMag) + ".")
+print("The location with the highest magnitude is " + str(topMag[0] + " at " + str(topMag[1]) + "."))
